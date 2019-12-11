1fbba528f9b4d11ff1f9b5d1dff23a0204087636
JalalMadhaji <jalal.almadhaji90@gmail.com>
2019-12-11 19:36:02 +0300

    Updates

---
+++ precache-manifest.17b48db41e07902c36759221d16b1e41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ba4f8b7fd07233940964b1aa39fb806",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "894e7013026eb603ccaf",
+    "url": "/weather-app/static/css/main.6783f406.chunk.css"
+  },
+  {
+    "revision": "d26065a5174728fb50b6",
+    "url": "/weather-app/static/js/2.833954fc.chunk.js"
+  },
+  {
+    "revision": "894e7013026eb603ccaf",
+    "url": "/weather-app/static/js/main.add3413e.chunk.js"
+  },
+  {
+    "revision": "9a1ba31ac3e696c22942",
+    "url": "/weather-app/static/js/runtime-main.3e53de6f.js"
+  },
+  {
+    "revision": "7de9a5166a5355c7299ee65ac8820ae4",
+    "url": "/weather-app/static/media/aerostat.7de9a516.jpg"
+  }
+]);
--- src/listItem.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class ListItem extends Component {
-    render() {
-        return (
-           <li onClick={()=>this.props.handleClick(this.props.name)}>{this.props.name}</li>
-        )
-    }
-}
